8634fa676b77d8825640a6dce496c3a45ea96125
fifimn <fifimaghfirotunnisa@gmail.com>
2021-01-27 13:23:19 +0700

    progress 27 januari 2021

---
+++ Garden's Tour(2)/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//public GameObject panelTittle;
+//public GameObject panelMainMenu;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame (){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
+   }
+
+   public void QuitGame(){
+       Debug.Log("QUIT");
+       Application.Quit();
+   }
+}
