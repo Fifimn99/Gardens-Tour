10cb931737865cd9232d77c31dedf556f4d1b866
fifimn <fifimaghfirotunnisa@gmail.com>
2021-01-30 10:43:10 +0700

    progress 30 Januari 2021

---
+++ Garden's Tour(2)/Assets/Scripts/Information.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Information : MonoBehaviour
+{
+    public GameObject Info_Kunyit; 
+    public GameObject Info_Jahe; 
+    public GameObject Info_Temulawak;
+    public GameObject Info_Lemon; 
+    public GameObject Info_Serai; 
+
+    void Start() {
+        Info_Kunyit.gameObject.SetActive(false);
+        Info_Jahe.gameObject.SetActive(false);
+        Info_Temulawak.gameObject.SetActive(false);
+        Info_Lemon.gameObject.SetActive(false);
+        Info_Serai.gameObject.SetActive(false);
+
+    }
+   public void activeInfo_Kunyit(){
+        Info_Kunyit.gameObject.SetActive(true);
+
+   }
+      public void activeInfo_Jahe(){
+        Info_Kunyit.gameObject.SetActive(true);
+
+   }
+      public void activeInfo_Temulawak(){
+        Info_Kunyit.gameObject.SetActive(true);
+
+   }
+      public void activeInfo_Lemon(){
+        Info_Kunyit.gameObject.SetActive(true);
+
+   }
+      public void activeInfo_Serai(){
+        Info_Kunyit.gameObject.SetActive(true);
+
+   }
+   public void CloseInfo(){
+
+   }
+}
